32cb98796166e85816f0312bd6b36a302d7a21ec
syao <syao.y40@gmail.com>
2020-03-15 20:19:40 -0400

    Fixed up code sample. Removed unused code.

--- app.js
+++ app.js
@@ -1,15 +1,3 @@
-let showMenu = () => {
-	document.getElementById('menu-icon').classList.add('menu-hide');
-	document.getElementById('close-icon').classList.remove('menu-hide');
-	document.getElementById('navbar').classList.remove('menu-hide');
-	 }
-
-let hideMenu = () => {
-	document.getElementById('menu-icon').classList.remove('menu-hide');
-	document.getElementById('close-icon').classList.add('menu-hide');
-	document.getElementById('navbar').classList.add('menu-hide');
-}
-
 let htmlDisplay = {
 	convertXML: (code) => code
 		.replace(/</g,'&lt;')
@@ -22,16 +10,13 @@ let htmlDisplay = {
 }
 
 document.addEventListener("DOMContentLoaded", (event) => {
-
 	document.querySelector('#html_display button[type="submit"]')
 		.addEventListener('click', () => 
 		htmlDisplay.populateOutput(htmlDisplay.convertXML(htmlDisplay.getInput())));
-
-	document.getElementById('menu-icon').addEventListener('click', showMenu);
 	
-	document.getElementById('close-icon').addEventListener('click', hideMenu);
-	
-	document.querySelectorAll('nav a').forEach((elem) => {
-		elem.addEventListener('click', hideMenu);
+	document.querySelectorAll('#navbar a').forEach((elem) => {
+		elem.addEventListener('click', () => 
+		document.querySelector('#navbar input[type="checkbox"]').checked = false
+		);
 	});
 });
